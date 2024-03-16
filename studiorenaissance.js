f2eb5d630f593a1bb07e963053f8a3ac3b0ecc8f
Irene <irebrulea@gmail.com>
2024-03-16 12:01:37 +0100

    wip

--- studiorenaissance.js
+++ studiorenaissance.js
@@ -2,9 +2,16 @@ document.addEventListener("DOMContentLoaded", function(){ // serve ad aspettare 
     let stato = false;
     document.getElementById("openMenuButtom").addEventListener("click",function(){// preparati all'evento click dell'elemento con ID open menu button ed esegue il codice all'interno di function quando il bottone viene premuto
         stato = !stato;
-        if(stato===false){
-            let menu=document.getElementsByClassName("hide")
+        let menu=document.getElementById("menuScomparsa")
+        if(stato===false)// se lo stato è false il menu deve scomparire
+        {
+         menu.classList.add("hide");
+         menu.classList.remove("show")
+        }else{
+            menu.classList.remove("hide");
+            menu.classList.add("show")
         }
+
     })
 
 })
